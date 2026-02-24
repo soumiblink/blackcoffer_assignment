e94a4bce582399b14a45755a06ee8ccc7f7f20fb
soumiblink <soumiblink@gmail.com>
2026-02-24 12:34:03 +0530

    Fix MongoDB Atlas connection

--- backend/server.js
+++ backend/server.js
@@ -7,7 +7,12 @@ const dataRoutes = require('./routes/dataRoutes');
 const app = express();
 
 // Connect to MongoDB
-connectDB();
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+})
+.then(() => console.log("MongoDB Connected"))
+.catch(err => console.error("MongoDB connection error:", err));
 
 // Middleware
 app.use(cors({
